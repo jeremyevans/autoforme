f97a1306f1c5e538a1dbe06e7c11b1ae38d4c6f8
Jeremy Evans <code@jeremyevans.net>
2013-11-04 10:05:53 -0800

    Make OptsAttribute#opts_attribute deal with blocks

--- lib/autoforme/utils.rb
+++ lib/autoforme/utils.rb
@@ -2,7 +2,15 @@ module AutoForme
   module OptsAttributes
     def opts_attribute(*meths)
       meths.each do |meth|
-        define_method(meth) do |*args|
+        define_method(meth) do |*args, &block|
+          if block
+            if args.empty?
+              opts[meth] = block
+            else
+              raise ArgumentError, "No arguments allowed if passing a block"
+            end
+          end
+
           case args.length
           when 0
             opts[meth] 
