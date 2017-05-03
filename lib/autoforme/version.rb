a11b3243142f66c2996e34ac6194f7f0b963ef31
Jeremy Evans <code@jeremyevans.net>
2017-05-03 14:09:39 -0700

    Bump version to 1.6.0

--- lib/autoforme/version.rb
+++ lib/autoforme/version.rb
@@ -2,7 +2,7 @@
 
 module AutoForme
   # Version constant, use <tt>AutoForme.version</tt> instead.
-  VERSION = '1.5.0'.freeze
+  VERSION = '1.6.0'.freeze
 
   # Returns the version as a frozen string (e.g. '0.1.0')
   def self.version
