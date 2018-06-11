10d0a1a4813d8662d3c0c33370b8231c463c7ba7
Jeremy Evans <code@jeremyevans.net>
2018-06-11 11:32:04 -0700

    Add MAJOR/MINOR/TINY/VERSION_NUMBER constants

--- lib/autoforme/version.rb
+++ lib/autoforme/version.rb
@@ -1,8 +1,22 @@
 # frozen-string-literal: true
 
 module AutoForme
+  # The major version of AutoForme, updated only for major changes that are
+  # likely to require modification to apps using AutoForme.
+  MAJOR = 1
+
+  # The minor version of AutoForme, updated for new feature releases of AutoForme.
+  MINOR = 7
+
+  # The patch version of AutoForme, updated only for bug fixes from the last
+  # feature release.
+  TINY = 0
+
   # Version constant, use <tt>AutoForme.version</tt> instead.
-  VERSION = '1.7.0'.freeze
+  VERSION = "#{MAJOR}.#{MINOR}.#{TINY}".freeze
+
+  # The full version of AutoForme as a number (1.8.0 => 10800)
+  VERSION_NUMBER = MAJOR*10000 + MINOR*100 + TINY
 
   # Returns the version as a frozen string (e.g. '0.1.0')
   def self.version
